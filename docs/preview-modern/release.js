93f647f1c834a599733b15ca3abbf729f7e15f8b
jdlc86 <julopezcardona@gmail.com>
2026-09-04 22:11:31 +0200

    release: promote build 2026.09.04.04

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.04.03",
-  released_at:"2026-09-04T13:30:00+02:00"
+  build:"2026.09.04.04",
+  released_at:"2026-09-04T22:00:00+02:00"
 });
