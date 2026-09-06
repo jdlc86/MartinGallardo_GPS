2877d9249ab5ada1ca4d7cdcaba970d3838ce048
jdlc86 <julopezcardona@gmail.com>
2026-09-06 21:31:18 +0200

    release: publish compact home layout build 2026.09.06.04

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.06.03",
-  released_at:"2026-09-06T20:48:00+02:00"
+  build:"2026.09.06.04",
+  released_at:"2026-09-06T21:29:00+02:00"
 });
