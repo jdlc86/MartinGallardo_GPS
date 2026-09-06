1092c1a98c2fa8e7c11b0a248fe43cfb09741817
jdlc86 <julopezcardona@gmail.com>
2026-09-06 21:52:07 +0200

    release: publish operational access-session fix 2026.09.06.06

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.06.05",
-  released_at:"2026-09-06T21:41:00+02:00"
+  build:"2026.09.06.06",
+  released_at:"2026-09-06T21:52:00+02:00"
 });
