e06042c712f6c9c5092b4f42d41cb9bb9cf5d656
jdlc86 <julopezcardona@gmail.com>
2026-09-06 16:12:08 +0200

    release: publish Mini App build 2026.09.06.01

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.04.04",
-  released_at:"2026-09-04T22:00:00+02:00"
+  build:"2026.09.06.01",
+  released_at:"2026-09-06T16:05:00+02:00"
 });
