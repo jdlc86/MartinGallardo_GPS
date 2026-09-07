f45b8a0ac4e7e1eb446f50bff6fa8b26323f919b
jdlc86 <julopezcardona@gmail.com>
2026-09-07 02:18:11 +0200

    release: publish DB health replication fix 2026.09.07.04

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.07.03",
-  released_at:"2026-09-07T02:11:00+02:00"
+  build:"2026.09.07.04",
+  released_at:"2026-09-07T02:24:00+02:00"
 });
