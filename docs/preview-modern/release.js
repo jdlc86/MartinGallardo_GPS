44e2954cc1d75233202377312f368cc957021ae5
jdlc86 <julopezcardona@gmail.com>
2026-09-08 13:59:33 +0200

    release: publish session expiry race fix

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.07.08",
-  released_at:"2026-09-07T20:30:00+02:00"
+  build:"2026.09.08.01",
+  released_at:"2026-09-08T13:58:00+02:00"
 });
