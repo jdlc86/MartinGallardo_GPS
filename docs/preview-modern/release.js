459ceba99ce68e0d79668c8ebd0a71e44972b9a7
jdlc86 <julopezcardona@gmail.com>
2026-09-08 15:20:04 +0200

    release: publish explicit pending-flow recovery

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.08.01",
-  released_at:"2026-09-08T13:58:00+02:00"
+  build:"2026.09.08.02",
+  released_at:"2026-09-08T15:20:00+02:00"
 });
