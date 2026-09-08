3e702e2e2bda73e15025b1ae6b9509e89b0de13f
jdlc86 <julopezcardona@gmail.com>
2026-09-09 00:18:08 +0200

    release: publish owner resource observability

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.08.03",
-  released_at:"2026-09-08T21:07:00+02:00"
+  build:"2026.09.09.04",
+  released_at:"2026-09-09T00:35:00+02:00"
 });
