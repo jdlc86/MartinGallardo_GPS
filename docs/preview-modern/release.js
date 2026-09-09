55ff0f8e05820fbb3d11c1d664477002e0a09e53
jdlc86 <julopezcardona@gmail.com>
2026-09-09 02:14:41 +0200

    release: bump Mini App to B07

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.09.06",
-  released_at:"2026-09-09T02:12:00+02:00"
+  build:"2026.09.09.07",
+  released_at:"2026-09-09T02:18:00+02:00"
 });
