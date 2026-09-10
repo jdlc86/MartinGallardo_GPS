d948329aa1f57e9a5b5feda53913440ad181e791
jdlc86 <julopezcardona@gmail.com>
2026-09-10 21:50:18 +0200

    release: roll out connectivity messages cache v102

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.10.01",
-  released_at:"2026-09-10T21:21:00+02:00"
+  build:"2026.09.10.02",
+  released_at:"2026-09-10T21:45:00+02:00"
 });
