f0c94c7ce391be1cb000bba2505158cd78ec2161
jdlc86 <julopezcardona@gmail.com>
2026-09-10 23:12:20 +0200

    release: expose build 2026.09.10.05

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.10.04",
-  released_at:"2026-09-10T22:55:00+02:00"
+  build:"2026.09.10.05",
+  released_at:"2026-09-10T23:10:00+02:00"
 });
