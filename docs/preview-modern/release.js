1b3f7ec31ab2d436e9e39e03da0ff8cbb3f27c71
jdlc86 <julopezcardona@gmail.com>
2026-09-24 03:24:37 +0200

    release: publish contextual control css fix

--- docs/preview-modern/release.js
+++ docs/preview-modern/release.js
@@ -1,6 +1,6 @@
 window.PMGRelease=Object.freeze({
   product:"ParkingMartin-G",
   version:"1.4.0",
-  build:"2026.09.24.07",
+  build:"2026.09.24.08",
   released_at:"2026-09-23T17:00:00+02:00"
 });
